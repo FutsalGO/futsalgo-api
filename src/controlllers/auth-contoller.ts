b55564b97ea0900b9505317ba8b654f1273d54e5
farisrahman674 <110735410+farisrahman674@users.noreply.github.com>
2025-08-22 23:25:03 +0700

    Feat 001 (#1)
    * [FEAT-001]: Implement user registration functionality
    
    * [REFACTOR-001]: Create auth-services and remove code from controller
    
    * [REFACTOR-001]: Adjust API response format in controller
    
    * [REFACTOR-001]: Configure path aliases and update import statements
    
    * [CHORE-001]: Update project dependencies and scripts

---
+++ src/controlllers/auth-contoller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { registerUser } from "@/services/auth-service";
+import { registerSchema } from "@/validations/auth-validation";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    // validasi input
+    const { error, value } = registerSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        code: 400,
+        status: "error",
+        message: "Validation error",
+        errors: error.details[0]?.message,
+      });
+    }
+
+    const { name, phone, email, password } = value;
+    const user = await registerUser(name, phone, email, password);
+
+    return res.status(201).json({
+      code: 201,
+      status: "success",
+      message: "User registered successfully",
+      data: {
+        id: user.name,
+        phone: user.phone,
+        email: user.email,
+        password: user.password,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ code: 500, status: "error", message: "Internal server error" });
+  }
+};
---
+++ src/services/auth-service.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import prisma from "@/prisma/client";
+
+export async function registerUser(
+  name: string,
+  phone: string | null,
+  email: string,
+  password: string
+) {
+  // manual validation (mirip logic Joi)
+  if (!name || name.length < 3 || name.length > 100) {
+    throw new Error("Name must be between 3 and 100 characters");
+  }
+
+  const phoneRegex = /^(?:\+62|62|0)[0-9]{9,13}$/;
+  if (phone && !phoneRegex.test(phone)) {
+    throw new Error("Phone number must be a valid Indonesian number");
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    throw new Error("Invalid email format");
+  }
+
+  if (!password || password.length < 6) {
+    throw new Error("Password must be at least 6 characters");
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // simpan ke database
+  const user = await prisma.user.create({
+    data: {
+      name,
+      phone,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return user;
+}

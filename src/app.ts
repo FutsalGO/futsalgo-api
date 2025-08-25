e28f04c94e83ec62b329c615734658ea840f3876
munaky <010905mn@gmail.com>
2025-08-25 11:25:27 +0700

    [FEAT-004]: add enpoint(read, create, delete) with validation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { cors } from "@/middlewares/cors-middleware";
 import { errorHandler } from "@/middlewares/error-handler-middleware";
 import authRoutes from "@/routes/auth-route";
+import userBookingRoutes from "@/routes/user/booking-route";
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors);
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings/user", userBookingRoutes);
 
 app.use(errorHandler);
 

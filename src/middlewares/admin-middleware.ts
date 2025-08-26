edfb263df86443e86c3f0a0a861df103bbcf3392
munaky <010905mn@gmail.com>
2025-08-26 11:59:49 +0700

    [FEAT-006]: implement admin booking management with CRUD operations and validation

---
+++ src/middlewares/admin-middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "@/utils/jwt"; // pastikan ini mengembalikan payload token yang sudah didecode
+
+export function requireAdmin(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies?.auth_token; // Ambil dari cookie, bukan header
+
+    if (!token) {
+        return res.status(401).json({
+            code: 401,
+            status: "unauthorized",
+            message: "Token tidak ditemukan. Silakan login terlebih dahulu.",
+        });
+    }
+
+    try {
+        const decoded = verifyToken(token);
+        if ((decoded as any).role != 'admin') {
+            return res.status(401).json({
+                code: 401,
+                status: "unauthorized",
+                message: "Anda tidak memiliki otorisasi admin.",
+            });
+        };
+
+        (req as any).user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            code: 401,
+            status: "unauthorized",
+            message: "Token tidak valid atau telah kedaluwarsa.",
+        });
+    }
+}

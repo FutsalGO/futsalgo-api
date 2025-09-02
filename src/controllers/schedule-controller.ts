ce52834d954bcbe2c6fae96c7b1c7441735c52f1
farisrahman674 <110735410+farisrahman674@users.noreply.github.com>
2025-09-02 09:42:32 +0700

    [FEAT-009]: Pull and Merge
    * [FEAT-003]: add GET /field endpoint to fetch all fields with sorting
    
    * [FEAT-003]: add GET /field endpoint to fetch all fields with sorting
    
    * [FEAT-003]: add new end point
    
    * [REFACTOR-003]: Change code in field-route.ts
    
    * [REFACTOR-003]: Rename field routes and controller to use plural naming
    
    * [REFACTOR-003]: Correct typo in import name
    
    * [REFACTOR-003]: move fields logic from controller to services layer
    
    * [FEAT-006]: implement admin booking management with CRUD operations and validation (#5)
    
    * [FEAT-007]: add nodemailer for booking confirmation emails, update booking status options (#6)
    
    * [FEAT-003]: add GET /field endpoint to fetch all fields with sorting
    
    * [FEAT-003]: add new end point
    
    * [REFACTOR-003]: Change code in field-route.ts
    
    * [REFACTOR-003]: Rename field routes and controller to use plural naming
    
    * [REFACTOR-003]: Correct typo in import name
    
    * [FEAT-007]: add nodemailer for booking confirmation emails, update booking status options
    
    * [FEAT-007]: refactor import path for email sender service in booking service
    
    ---------
    
    Co-authored-by: farisrahman674 <farisrahman674@gmail.com>
    Co-authored-by: farisrahman674 <110735410+farisrahman674@users.noreply.github.com>
    
    * [FEAT-008]: add schedule endpoint, fix error handler middleware (#8)
    
    * [FEAT-008]: add schedule endpoint, fix error handler middleware
    
    * [FEAT-008]: adjust schedule time range to start from 8 AM to 10 PM
    
    * Feat 008 (#9)
    
    * [FEAT-008]: update getSchedules to accept field_id parameter and modify route to include field_id
    
    * [FEAT-009]: crud field, add multer on field
    
    ---------
    
    Co-authored-by: munaky <121778708+munaky@users.noreply.github.com>
    Co-authored-by: Muh. Yaumil Aksah Hamid <assperacus77@gmail.com>
    Co-authored-by: yaumil Aksah <76608198+AxVsel@users.noreply.github.com>

---
+++ src/controllers/schedule-controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { getSchedules } from "@/services/schedule-service";
+
+export async function handleGetSchedules(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const field_id = Number(req.params.field_id);
+    const schedules = await getSchedules(field_id);
+
+    return res.status(200).json({
+      code: 200,
+      status: "success",
+      message: "Booking Schedules retrieved successfully",
+      data: schedules,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
